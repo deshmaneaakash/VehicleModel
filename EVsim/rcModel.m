e855048eec97b8fd79b92a24020615f07d45c69c
Aakash Deshmane <deshmaneaakash@tamu.edu>
2024-02-08 18:46:55 -0600

    fix rcmodel

--- EVsim/rcModel.m
+++ EVsim/rcModel.m
@@ -1,5 +1,6 @@
-function ccv = rcModel(ocv, current, t)
+function [ccv, vP]  = rcModel(ocv, current, t, vP)
 
+r0 = 1;
 r1 = 10;
 r2 = 5;
 c1 = 12;
@@ -8,17 +9,18 @@ c2 = 10;
 % time = linspace(0, 100, 100);
 
 
-vP = [0 0];
+% vP = [0 0];
 % for i = 1:length(time)
     % t = time(i);
     % current = interp1(currentProfile_t, currentProfile_A, t, "linear");
 
+V0  = r0 * current;
 vP1 = RCP(current, r1, c1, vP(1), t);
 vP2 = RCP(current, r2, c2, vP(2), t);
 
-% vP(1) = vP1;
-% vP(2) = vP2;
-ccv = ocv + vP1 + vP2;
+vP(1) = vP1;
+vP(2) = vP2;
+ccv = ocv + V0 + vP1 + vP2;
 
 % end
 
