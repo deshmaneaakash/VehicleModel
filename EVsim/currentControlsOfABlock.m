11d5f1435ce4d50b13f3d55062c9d2e52ba2e832
Aakash Deshmane <deshmaneaakash@tamu.edu>
2024-02-14 18:18:42 -0600

    PID controller of a thermal mass

---
+++ EVsim/currentControlsOfABlock.m
@@ -0,0 +1,92 @@
+%% CURRENT CONTROLS FROM THERMAL MODELING
+% This script demonstrates a PID regulator on a current controller through
+% a block of copper. 
+
+clear
+clc
+
+lengthOfBlock  = 0.1; % m
+breadthOfBlock = 0.03; % m
+widthOfBlock   = 0.05; % m
+volumeOfBlock = lengthOfBlock * breadthOfBlock * widthOfBlock; % m3
+densityOfBlock = 2700; % kg/m3
+massOfBlock = densityOfBlock * volumeOfBlock; % kg
+dt = 1; % sec
+resistivityOfCopper = 2.82e-8; % ohm-m
+rBlock = resistivityOfCopper * lengthOfBlock / (breadthOfBlock * widthOfBlock);
+cP = 902; % Specific heat capacity of copper in J/kgC
+tAmbient = 27; % deg C
+initialTemp = 27; % deg C
+rToAmbient = 10; % C/W
+% current = 1000; % A
+
+simulationTimeLimit = 3600 * 5; % sec
+simulationTime = 1:simulationTimeLimit;
+
+% Controls
+referenceTemp = 40;
+kp = 100;
+ki = 0.048;
+kd = 0;
+
+% Simulation
+for time = simulationTime
+    if time == 1
+        currentTemp = initialTemp;
+        prevITerm = 0;
+        prevError = 0;
+    else
+        currentTemp = Tt(time - 1);
+    end
+    
+    [current, prevITerm, prevError, pTerm, dTerm] = pid(dt, referenceTemp, currentTemp, kp, ki, kd, prevError, prevITerm);
+    Tt(time) = ((current^2) * rBlock * rToAmbient + massOfBlock*cP*rToAmbient*currentTemp + tAmbient) / ...
+                    ( 1 + massOfBlock*cP*rToAmbient);
+    currentVector(time) = current;
+    errorVector(time) = prevError;
+    pTermVector(time) = pTerm;
+    iTermVector(time) = prevITerm;
+    dTermVector(time) = dTerm;
+
+end
+
+figure(1)
+hold on
+grid on
+plot(simulationTime / 3600, Tt,"LineWidth", 2)
+yline(40)
+xlabel('Time [hrs]')
+ylabel('Temperature of the block')
+title('Temperature response')
+
+figure
+grid on
+plot(simulationTime / 3600, currentVector,"LineWidth", 2)
+xlabel('Time [hrs]')
+ylabel('Current of the block')
+title('Current response')
+
+% figure
+% grid on
+% hold on
+% plot(simulationTime / 3600, errorVector,"LineWidth", 2, "DisplayName", 'Error')
+% plot(simulationTime / 3600, pTermVector,"LineWidth", 2, "DisplayName", 'P term')
+% plot(simulationTime / 3600, iTermVector,"LineWidth", 2, "DisplayName", 'I term')
+% xlabel('Time [hrs]')
+% ylabel('Error')
+% title('Error response')
+% legend
+
+%% LOCAL FUNCTIONS
+
+function [output, iTerm, error, pTerm, dTerm] = pid(dt, reference, state, kp, ki, kd, prevError, prevITerm)
+
+    error = reference - state;
+    pTerm = kp * error;
+    iTerm = ki * error + prevITerm;
+    dTerm = kd * (error - prevError) / dt;
+    output = pTerm + iTerm + dTerm;
+end
+
+
+
