f7f0a10a8327532ff069c852109b3e08a877bc18
Aakash Deshmane <deshmaneaakash@tamu.edu>
2024-02-15 12:15:06 -0600

    added 3 and more rc pairs

--- EVsim/rcModelMock.m
+++ EVsim/rcModelMock.m
@@ -1,37 +1,53 @@
 clear
 clc
 
-r1 = 17.44 * 0.001;
-r2 = 5;
-c1 = 12;
-c2 = 10;
+r0 = 4;
+r1 = 4;
+r2 = 4;
+r3 = 4;
+
+c1 = 50;
+c2 = 50;
+c3 = 50;
+
+rcParams = [6.2  6.2;
+            35 35];
+
 ocv = 3;
 
 time = linspace(0, 100, 100);
 currentProfile_t = [0 25 26 50 51 100];
 currentProfile_A = [0 0 1 1 0 0] * 0.5;
+numPairs = 2;
 
-vP = [0 0];
+vP = zeros(1, numPairs);
+
 for i = 1:length(time)
     t = time(i);
     current = interp1(currentProfile_t, currentProfile_A, t, "linear");
+    
+    % Iterating through pairs
+    ccv(i) = ocv;
 
-    vP1 = RCP(current, r1, c1, vP(1), t);
-    vP2 = RCP(current, r2, c2, vP(2), t);
+    for rcPair = 1:numPairs
+        r = rcParams(1, rcPair);
+        c = rcParams(2, rcPair);
+        vPolar = RCP(current, r, c, vP(rcPair), t);
+        ccv(i) = ccv(i) + vPolar;
+        vP(rcPair) = vPolar;
+    end
 
-    vP(1) = vP1;
-    vP(2) = vP2;
-    ccv(i) = ocv + vP1 + vP2;
-
+    ccv(i) = ccv(i) + current * r0;
 end
 
-figure
-grid
+figure(1)
+hold on
+grid on
 plot(time, ccv, "LineWidth", 2)
 xlabel("Time [s]")
 ylabel("Voltage [V]")
 title("RC Pair Response")
-ylim([2 20])
+% ylim([2 20])
 
 function v = RCP(current, r, c, vP, t)
     rcExp = exp(-t / (r * c));
