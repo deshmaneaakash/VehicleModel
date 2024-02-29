04f935f8c955a0b65fc80af53cde5d5a557b86fe
Aakash Deshmane <deshmaneaakash@tamu.edu>
2024-02-28 23:12:52 -0600

    rc model code block for simulink

---
+++ EVsim/rcModelForParamEstimation.m
@@ -0,0 +1,16 @@
+function [ccv, vP]  = rcModelForParamEstimation(ocv, current, r0, r1, c1, r2, c2, dt, vP)
+
+    V0  = r0 * current;
+    vP1 = RCP(current, r1, c1, vP(1), dt);
+    vP2 = RCP(current, r2, c2, vP(2), dt);
+    
+    vP(1) = vP1;
+    vP(2) = vP2;
+    ccv = ocv + V0 + vP1;
+
+end
+
+function v = RCP(current, r, c, vP, dt)
+    rcExp = exp(-dt / (r * c));
+    v = ((current * r) * (1 - rcExp) + (vP * rcExp));
+end
