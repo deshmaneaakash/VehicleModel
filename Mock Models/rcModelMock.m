b03d675b68a68818d5629049ad276f0b2f6111c4
Aakash Deshmane <deshmaneaakash@tamu.edu>
2024-03-13 13:24:53 -0400

    added discrete rc model in mock models

--- Mock Models/rcModelMock.m
+++ Mock Models/rcModelMock.m
@@ -2,22 +2,22 @@ clear
 clc
 
 r0 = 0.01;
-rcParams = [0.02 0.03;
-            30 40];
+rcParams = [0.03 0.04;
+            60 60];
 ocv = 3;
 
-load('C:\Users\deshm\OneDrive\Documents\GitHub\VehicleModel\EVsim\Panasonic-18650PF-Data-master\Panasonic 18650PF Data\25degC\5 pulse disch\03-11-17_08.47 25degC_5Pulse_HPPC_Pan18650PF.mat')
+load('C:\Users\deshm\OneDrive\Documents\GitHub\VehicleModel\Data\Panasonic-18650PF-Data-master\Panasonic 18650PF Data\25degC\5 pulse disch\03-11-17_08.47 25degC_5Pulse_HPPC_Pan18650PF.mat')
 
-time = meas.Time;
-voltage = meas.Voltage;
-currentData = meas.Current;
+% time = meas.Time;
+% voltage = meas.Voltage;
+% currentData = meas.Current;
 
-% time = linspace(0, 100, 100);
-% currentProfile_t = [0 25 26 50 51 100];
-% currentProfile_A = [0 0 1 1 0 0] * -0.5;
+time = linspace(0, 100, 100);
+currentProfile_t = [0 25 26 50 51 100];
+currentProfile_A = [0 0 1 1 0 0] * 0.5;
 
-currentProfile_t = time;
-currentProfile_A = currentData;
+% currentProfile_t = time;
+% currentProfile_A = currentData;
 
 numPairs = 2;
 vP = zeros(1, numPairs);
@@ -30,8 +30,8 @@ for i = 1:length(time)
         dt = time(i) - time(i-1);
     end
 
-    % current = interp1(currentProfile_t, currentProfile_A, t, "linear");
-    current = currentData(i);
+    current = interp1(currentProfile_t, currentProfile_A, t, "linear");
+    % current = currentData(i);
 
     % Iterating through pairs
     ccv(i) = ocv;
@@ -58,6 +58,12 @@ title("RC Pair Response")
 % xlim([15545 15560])
 
 function v = RCP(current, r, c, vP, dt)
+    % Continous model
     rcExp = exp(-dt / (r * c));
     v = ((current * r) * (1 - rcExp) + (vP * rcExp));
+
+    % Discrete Model
+    % dV = dt*(-vP / (r * c) + current / c);
+    % v = vP + dV;
+
 end
